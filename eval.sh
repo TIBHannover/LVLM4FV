7a520ecc0f77fa09f65c47f113483f50cecf3e10
sahartahmasebi <79540116+sahartahmasebi@users.noreply.github.com>
2025-11-24 10:45:31 +0100

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,14 +1,14 @@
 ######### evidence retrieval
 #image retrieval
-python eval_ir_llms.py --mode=test --media=img --model_name==Salesforce/instructblip-flan-t5-xl --use_llm_score=False --mocheg_result_path=checkpoints/mocheg_img_results/query_result_img.pkl --prompt=Is this image and text query mentioning the same person or topic? answer with yes or no.
+python eval_ir_llms.py --mode=test --media=img --model_name==Salesforce/instructblip-flan-t5-xl --use_llm_score=False --mocheg_result_path=checkpoints/mocheg_img_results/query_result_img.pkl --prompt="Is this image and text query mentioning the same person or topic? answer with yes or no."
 #Text retrieval
-python eval_ir_llms.py --mode=test --media=txt --model_name==Mistral-7B-OpenOrca --use_llm_score=True --mocheg_result_path=checkpoints/mocheg_txt_results/query_result_txt.pkl --prompt=Is this corpus related to the query? Answer with yes or no.
+python eval_ir_llms.py --mode=test --media=txt --model_name==Mistral-7B-OpenOrca --use_llm_score=True --mocheg_result_path=checkpoints/mocheg_txt_results/query_result_txt.pkl --prompt="Is this corpus related to the query? Answer with yes or no."
 
 
 
 ######## Fact verification
 # with text evidence
-python FNdetection_llm.py --media=txt --model_name=Open-Orca/Mistral-7B-OpenOrca --evidence_type=gold --two_level_prompting=False --task1_out=retrieval/output/ir_llms/mocheg/txt --prompt=Does this evidence support or refute this claim?answer with yes if it supports, answer with no if it refutes and answer with none if it does not provide enough information.
+python FNdetection_llm.py --media=txt --model_name=Open-Orca/Mistral-7B-OpenOrca --evidence_type=gold --two_level_prompting=False --task1_out=retrieval/output/ir_llms/mocheg/txt --prompt="Does this evidence support or refute this claim?answer with yes if it supports, answer with no if it refutes and answer with none if it does not provide enough information."
 # with multimodal evidence
-python FNdetection_llm.py --media=multimodal --model_name=llava-hf/llava-v1.6-mistral-7b-hf --evidence_type=gold --two_level_prompting=False --task1_out=retrieval/output/ir_llms/mocheg/txt --task1_out_img==retrieval/output/ir_llms/mocheg/img --prompt=Does this evidence support or refute this claim?answer with yes if it supports, answer with no if it refutes and answer with none if it does not provide enough information.
+python FNdetection_llm.py --media=multimodal --model_name=llava-hf/llava-v1.6-mistral-7b-hf --evidence_type=gold --two_level_prompting=False --task1_out=retrieval/output/ir_llms/mocheg/txt --task1_out_img==retrieval/output/ir_llms/mocheg/img --prompt="Does this evidence support or refute this claim?answer with yes if it supports, answer with no if it refutes and answer with none if it does not provide enough information."
 
